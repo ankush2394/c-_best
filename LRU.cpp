501050c08f70b0df3914f44a694f986d811f4eaf
Ankush Virmani <ankush.virmani@olx.com>
2022-02-23 10:37:01 +0530

    adding C++ important codes

--- LRU.cpp
+++ LRU.cpp
@@ -10,31 +10,27 @@ using namespace std;
 
 
 int cacheSize = 3;
-list<int> dq; //doubly link list
-unordered_map<int, bool> mp;   //check if the key already exists in cche or not
 unordered_map<int, list<pair<int,int>>::iterator> mp1;
 list<pair<int,int>> dq1;
 
-void insert(int x) {
+void insert(int key, int value) {
     //whenever you have to insert in the list , check if it already exists or not
 
-    if (mp.find(x) != mp.end()) {
-        //already in map cache
-        int last = dq.back();
-        dq.push_front(last);
-        dq.pop_back();
-
+    if (mp1.find(key) != mp1.end()) {
+        //it exists in cache
+        auto it = mp1[key];
+        dq1.erase(it);
+        dq1.push_front(make_pair(key, value));
+        mp1[key] = dq1.begin();
     } else {
-        //not in cache
-        if (dq.size()<cacheSize) {
-            dq.push_front(x);
-        } else {
-            int last = dq.back();
-            dq.push_front(x);
-            dq.pop_back();
-            mp.erase(last);
+        //doesnt exist in cache
+        dq1.push_front(make_pair(key, value));
+        mp1[key]=dq1.begin();
+
+        if(dq1.size()>cacheSize) {
+            mp1.erase(mp1.find(dq1.back().first));
+            dq1.pop_back();
         }
-        mp[x]=true;
     }
 }
 
@@ -44,13 +40,13 @@ int get(int key) {
         return -1;
     }
 
-    dq1.back();
+
     auto it = mp1[key];
+    int val = it->second;
     dq1.erase(it);
-    dq1.push_back(make_pair(key, it->second));
-
-
-    return mp[key];
+    dq1.push_front(make_pair(key, val));
+    mp1[key] = dq1.begin();
+    return val;
 
 }
 
